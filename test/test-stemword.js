6a9e7a167de127c1687d1ea9375f46828fc68389
Harold Thetiot <hthetiot@gmail.com>
2014-08-05 20:00:53 -0700

    implment english and french unit tests

--- test/test-stemword.js
+++ test/test-stemword.js
@@ -1,6 +1,19 @@
 var snowball = require('../index');
 
+function testStemWords(test, lang) {
+
+	var words = require("./" + lang + ".json");
+    var wordsIndex = Object.keys(words);
+
+	wordsIndex.forEach(function (word) {
+		test.equal(snowball.stemword(word, lang), words[word]);
+	});
+}
+
 exports['stemword'] = function (test) {
-    test.equal(snowball.stemword('consignment'), "consignm");
+
+    testStemWords(test, "english");
+    testStemWords(test, "french");
+
     test.done();
 };
